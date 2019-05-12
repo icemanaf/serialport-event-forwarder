6417cafe09f3e9cd5b6a04c6e51e6457b299aecf
icemanaf <icemanaf>
2019-05-12 01:18:16 +0100

    added readme,fixed bug when setting the Baud rate through the docker envorinmental variabe

--- config.js
+++ config.js
@@ -7,7 +7,13 @@ config.serial={};
 config.kafka.broker=process.env.KAFKA_BROKER|| "192.168.0.85:9092";
 config.kafka.topic=process.env.KAFKA_TOPIC||"PIR_DETECT";
 
+if (process.env.BAUD){
+    config.serial.baudrate=parseInt(process.env.BAUD);
+}else{
+    config.serial.baudrate=600;
+}
+
 config.serial.port=process.env.SERIAL_PORT || "COM3";
-config.serial.baudrate=process.env.BAUD || 596;
+
 
 module.exports=config;
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
-#IOT sensor data forwarder.
+# IOT sensor data forwarder.
+
+This application forwards on data received by a cheap 433Mhz RF module into a kafka broker.
